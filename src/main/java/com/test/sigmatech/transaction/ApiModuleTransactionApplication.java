98a74107d6acc2c91c68e9db11501a4ec938f2c7
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-02-02 19:09:45 +0700

    initial project

---
+++ src/main/java/com/test/sigmatech/transaction/ApiModuleTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.test.sigmatech.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModuleTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiModuleTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sigmatech/transaction/ApiModuleTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sigmatech.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModuleTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
